cc9b3d4614a3cf160332bb95157de455e9476ee8
LiteOnE <r89534217610@gmail.com>
2017-01-15 14:59:10 +0300

    Create Extended_Steam_Activity_Feed.user.js

---
+++ Extended_Steam_Activity_Feed.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Extended Steam Activity Feed
+// @namespace    http://tampermonkey.net/
+// @version      0.3(Beta)
+// @description  This script allows you posting statuses about apps using their IDs. Also you can now rate up all posts in your activity feed in 2 clicks.
+// @author       Lite_OnE
+// @match        http://steamcommunity.com/*/*/home/
+// @grant        unsafeWindow
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// ==/UserScript==
+
+var n = 0,        //number of posts on page
+    i = 0,        //counter
+    m_RUD = null; //m_RUD - modal Rate Up Dialog
+
+
+unsafeWindow.PostStatus = function () {
+    $.post($('.blotter_avatar_holder').find('a:first').attr('href') + "/ajaxpostuserstatus/", { sessionid: g_sessionID, status_text: $('#blotter_statuspost_textarea').val(), appid: $('#Status_AppID').val()});
+    alert('Posted!');
+    location.reload();
+};
+
+unsafeWindow.init_RateUpEvrthng = function() {
+    if (confirm('Click "Yes" to start rating up everything')) {
+        m_RUD = ShowBlockingWaitDialog("Executing", "Rated up: " + i + " posts of " + n);
+        n = $('[id*="vote_up_"]').length;
+        RateUpEvrthng(i);
+    }
+};
+
+function RateUpEvrthng (i){
+    if (i<n){
+        eval($('[id*="vote_up_"]:eq(' + i + ')').attr('onclick').split(' ')[1]);
+        m_RUD.Dismiss();
+        m_RUD = ShowBlockingWaitDialog("Executing", "Rated up: " + i+1 + " posts of " + n);
+        i++;
+        RateUpEvrthng(i);
+    }else{
+        //$.ajax({url : g_BlotterNextLoadURL, success: alert('0')})
+        $("html, body").scrollTop($(document).height());
+        setTimeout(function (){
+            if (confirm('All posts on this page are rated up! Want to process next page?')){
+                n = $('[id*="vote_up_"]').length;
+                RateUpEvrthng(i);
+            }else{m_RUD.Dismiss();}
+        }, 3000);
+    }
+}
+
+jQuery(document).ready(function() {
+    var emoticon_container = $(".emoticon_container").html();
+    $(".blotter_status_submit_ctn:first").replaceWith( '<div id="extended_af_container" style="width:100%;"><div class="profile_options_divider"></div><div style="float: right;" onclick="javascript:PostStatus();" class="btn_darkblue_white_innerfade btn_small"><span>Post status</span></div><span class="emoticon_container">'+ emoticon_container + '</span><input type="text" id="Status_AppID" placeholder="Input AppID" style="width: 75%; font-style: italic; text-align: center;"></div><div class="profile_options_divider"></div><div style="width: 100%; text-align: center;" onclick="javascript:init_RateUpEvrthng();" class="btn_darkblue_white_innerfade btn_small"><span>Rate up everything</span></div>');
+    $(".emoticon_container:first").css({"float": "right", "width":"7%"});
+});
