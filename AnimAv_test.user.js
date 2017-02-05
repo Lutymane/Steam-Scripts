a46e8ccae386e382ca2475ad528a343d2cc6b2d7
LiteOnE <r89534217610@gmail.com>
2017-02-05 20:17:03 +0300

    Update AnimAv_test.user.js

--- AnimAv_test.user.js
+++ AnimAv_test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Animated Avatar
 // @namespace    http://tampermonkey.net/
-// @version      1.3.3.7
+// @version      2.3.3.7
 // @description  Delite is nubas
 // @author       Lite_OnE
 // @match        *://steamccommunity.com/id/*/
@@ -9,29 +9,5 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
-var i=0,
-    j=100,    //number of iterations
-    p=1500,   //pause for a()
-    p2=1000;  //pause for b()
 
-function a(i){
-    if (i<j){
-        setTimeout(function (){$.post('http://steamcommunity.com/games/471220/selectAvatar', { sessionid: g_sessionID, selectedAvatar: 0});}, p);
-        setTimeout(function (){$.post('http://steamcommunity.com/games/471220/selectAvatar', { sessionid: g_sessionID, selectedAvatar: 1});}, 2*p);
-        setTimeout(function (){$.post('http://steamcommunity.com/games/471220/selectAvatar', { sessionid: g_sessionID, selectedAvatar: 2});}, 3*p);
-        setTimeout(function (){$.post('http://steamcommunity.com/games/471220/selectAvatar', { sessionid: g_sessionID, selectedAvatar: 3});}, 4*p);
-        setTimeout(function (){a(i++);}, 5*p);
-    }
-    return;
-}
-
-function b(i){
-    if (i<j){
-        setTimeout(function (){$.post('http://steamcommunity.com/games/257510/selectAvatar', { sessionid: g_sessionID, selectedAvatar: 4});}, p2);
-        setTimeout(function (){$.post('http://steamcommunity.com/games/401710/selectAvatar', { sessionid: g_sessionID, selectedAvatar: 8});}, 2*p2);
-        setTimeout(function (){b(i++);}, 3*p2);
-    }
-    return;
-}
-
-$(document).ready(function(){b(i);});  //or a() or nubas() whatever
+$(document).ready(function(){alert('Ha-ha!')});  
