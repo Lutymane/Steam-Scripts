378b146ef9528844707b3e15a29bbf4a8486be10
LiteOnE <r89534217610@gmail.com>
2017-02-05 20:35:27 +0300

    Update AnimAv_test.user.js

--- AnimAv_test.user.js
+++ AnimAv_test.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Animated Avatar
 // @namespace    http://tampermonkey.net/
-// @version      2.3.3.7
+// @version      3.3.3.7
 // @description  Delite is nubas
 // @author       Lite_OnE
-// @match        *://steamccommunity.com/id/*/
+// @match        *://steamcommunity.com/id/*/
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
