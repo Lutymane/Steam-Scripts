50dd0a103c97ad6886045ce92ed0392c5a859d47
LiteOnE <r89534217610@gmail.com>
2017-02-16 18:56:45 +0300

    Update AnimatedAvatar.test.user.js

--- AnimatedAvatar.test.user.js
+++ AnimatedAvatar.test.user.js
@@ -10,7 +10,6 @@
 
 var p = 1000,   //!Dont modify this value or you will get banned!
     stop_flag,  //stop flag .-.
-    vip = [0, 1, 2, 1, 263860, 0],
     ad = [ [471220, 4], [269570, 8], [526800, 5], ['NinjasPlusPlus', 12], ['SuperDistro', 8], [453830, 7], ['sentris', 29], [538410, 6], [536470, 4], [432330, 20] ], //avatars data
     cp, //choosed preset
     data_1 = '<a class="btn_profile_action btn_medium" id="disable_av"><span style="color: #E34234; display:none;">Disable Animated Avatar</span></a><a class="btn_profile_action btn_medium" id="enable_av"><span style="color: #01B1AF; display: block">Enable Animated Avatar</span></a>',
@@ -21,27 +20,13 @@ var p = 1000,   //!Dont modify this value or you will get banned!
 function preset(i){
     if (stop_flag===0){
         if (i<ad[cp][1]){
-            $.post('https://steamcommunity.com/games/' + ad[cp][0] + '/selectAvatar', { sessionid: g_sessionID, selectedAvatar: i});
+            $.post('http://steamcommunity.com/games/' + ad[cp][0] + '/selectAvatar', { sessionid: g_sessionID, selectedAvatar: i});
             setTimeout(function (){
                 i++;
                 preset(i);
             }, p);
         }else{
             preset(0);
-        }
-    }
-}
-
-function presetVIP(x){
-    if(stop_flag===0){
-        if (x<4){
-            $.post('https://steamcommunity.com/games/' + vip[4] + '/selectAvatar', { sessionid: g_sessionID, selectedAvatar: vip[x]});
-            setTimeout(function (){
-                x++;
-                presetVIP(x);
-            }, 800);
-        }else{
-            presetVIP(0);
         }
     }
 }
@@ -78,13 +63,4 @@ $(document).ready(function(){
         $('#disable_av').find('span').css({"display":"none"});
         $('#enable_av').find('span').text('Enable Animated Avatar');
     });
-    if (vip[5]==1){
-        $('#avatar_presets').append('<a class="popup_menu_item  btn_profile_action" id="vip" style="text-align: center">VIP Preset</a>');
-        $('#vip').click(function() {
-            $('#enable_av').find('span').css({"display":"none"});
-            $('#disable_av').find('span').css({"display":"block"});
-            stop_flag=0;
-            presetVIP(0);
-        });
-    }
 });
