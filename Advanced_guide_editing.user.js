361a9c8200b29b2fcb417b4af64c15cda96a200f
LiteOnE <r89534217610@gmail.com>
2017-03-16 22:52:08 +0300

    Update Advanced_guide_editing.user.js

--- Advanced_guide_editing.user.js
+++ Advanced_guide_editing.user.js
@@ -20,9 +20,9 @@ $(".btn_green_white_innerfade.btn_small_thin").attr("href", "javascript:Validate
 unsafeWindow.ValidateEditAdv = function () {
     $.post("http://steamcommunity.com/sharedfiles/setguidesubsection", {
         id : url.split("?")[1].split("&")[0].split("=")[1],
-		sectionid : url.split("?")[1].split("&")[1].split("=")[1],
-		sessionid : g_sessionID,
-		title : title.value,
-		description : description.value});
+	sectionid : url.split("?")[1].split("&")[1].split("=")[1],
+	sessionid : g_sessionID,
+	title : title.value,
+	description : description.value});
     alert ('Done! Now you can reload the page!');
 };
