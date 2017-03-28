2a01624cd804d2ae75ae6d978057f5131ccb9b85
LiteOnE <r89534217610@gmail.com>
2017-03-28 23:20:25 +0300

    Update BadgeAutocraft2.user.js

--- BadgeAutocraft2.user.js
+++ BadgeAutocraft2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Badge Autocraft 2
 // @namespace    *steamcommunity.com/
-// @version      2.1.3
+// @version      2.1.4
 // @description  Inspired by 10101000's Steam-AutoCraft. It works now by sending post requests, no more page loads
 // @author       Lite_OnE
 // @match        http*://steamcommunity.com/id/*/badges/
@@ -77,7 +77,7 @@ function ToggleAutocraft(i){
     
     if (!IsInBlackList(CurrentAppID))
     {
-        $.post('http://steamcommunity.com/profiles/' + g_steamID + '/ajaxcraftbadge/', {
+        $.post('https://steamcommunity.com/profiles/' + g_steamID + '/ajaxcraftbadge', {
         appid: CurrentAppID,
         series: 1,
         border_color: border,
