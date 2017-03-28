80e6a1bf70a81fbcd78cf304b982ec16a93ccea3
LiteOnE <r89534217610@gmail.com>
2017-03-28 22:43:50 +0300

    Update BadgeAutocraft2.user.js

--- BadgeAutocraft2.user.js
+++ BadgeAutocraft2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Badge Autocraft 2
 // @namespace    *steamcommunity.com/
-// @version      2.1.1
+// @version      2.1.2
 // @description  Inspired by 10101000's Steam-AutoCraft. It works now by sending post requests, no more page loads
 // @author       Lite_OnE
 // @match        http*://steamcommunity.com/id/*/badges/
