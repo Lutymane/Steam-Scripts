34aaf3608f0df21746eab3c0090871caa313d399
LiteOnE <r89534217610@gmail.com>
2017-06-23 16:26:32 +0300

    Update Extended-guide-editing.user.js

--- Extended-guide-editing.user.js
+++ Extended-guide-editing.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Extended guide editing
 // @namespace    http://steamcommunity.com/
-// @version      1.0
-// @description  Empty sectiion title + up to 1 million characters in one section
+// @version      1.1
+// @description  Empty section title + up to 1 million characters in one section
 // @author       Lite_OnE
 // @match        *://steamcommunity.com/sharedfiles/editguidesubsection/*
 // @grant        unsafeWindow
@@ -10,19 +10,25 @@
 // ==/UserScript==
 
 var url = $(location).attr('href').replace('#', '');
+var protocol;
 
-if ($(location).attr('href').split(':')[0]=='https'){
-    $(location).attr('href', 'http:'+$J(location).attr('href').split(':')[1]); //redirect to http protocol
+if (url.split(':')[0]=='https')
+{
+    protocol = "https";
+}
+else
+{
+    protocol = "http";
 }
 
-$(".btn_green_white_innerfade.btn_small_thin").attr("href", "javascript:ValidateEditAdv();");
+$(".btn_green_white_innerfade.btn_small_thin").attr("href", "javascript:ValidateEditExt();");
 
-unsafeWindow.ValidateEditAdv = function () {
-    $.post("http://steamcommunity.com/sharedfiles/setguidesubsection", {
+unsafeWindow.ValidateEditExt = function () {
+    $.post(protocol + "://steamcommunity.com/sharedfiles/setguidesubsection", {
         id : url.split("?")[1].split("&")[0].split("=")[1],
 	sectionid : url.split("?")[1].split("&")[1].split("=")[1],
 	sessionid : g_sessionID,
 	title : title.value,
 	description : description.value});
-    alert ('Done! Now you can reload the page!');
+    ShowAlertDialog( '', 'Done! Now you can reload the page!');
 };
