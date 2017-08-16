d9c417824ff1f8f1890334b67dcf28061813f586
LiteOnE <r89534217610@gmail.com>
2017-08-16 04:18:05 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-var g = $J.post("http://steamcommunity.com/id/*/ajaxenableprofilemodifier/", {
+$J.post("http://steamcommunity.com/id/*/ajaxenableprofilemodifier/", {
   sessionid: g_sessionID,
   appid: "753",
   assetid: "1309377320",
-  enabled: "1"}).done(function() { alert(g.responseText) });
+  enabled: "1"}).done(function(data) { alert(data.responseText) });
