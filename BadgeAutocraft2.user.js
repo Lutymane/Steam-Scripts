42009f8fb7990d243f22fd3412b934df232684c5
LiteOnE <r89534217610@gmail.com>
2017-08-19 01:57:34 +0300

    Update BadgeAutocraft2.user.js

--- BadgeAutocraft2.user.js
+++ BadgeAutocraft2.user.js
@@ -2,7 +2,7 @@
 // @name         Badge Autocraft 2
 // @namespace    *steamcommunity.com/
 // @version      2.3.4
-// @description  Thanks to Psy0ch and MrSteakPotato for testing! Inspired by 10101000's Steam-AutoCraft. Allows you to craft remaining badges in one click.
+// @description  Thanks to Psy0ch and MrSteakPotato for testing! Inspired by 10101000's Steam-AutoCraft. Allows you to craft remaining badges in one click. Works much more faster, takes much less resources.
 // @author       Lite_OnE
 // @match        *steamcommunity.com/*/*/badges*
 // @supportURL   https://github.com/LiteOnE/Steam-Scripts/issues
