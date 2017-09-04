a3e165f7617bfcf6bbbe87c8b94b93e7539faa76
LiteOnE <r89534217610@gmail.com>
2017-09-04 17:53:21 +0300

    Delete test.js

--- test.js
+++
@@ -1,5 +0,0 @@
-$J.post("http://steamcommunity.com/id/*/ajaxenableprofilemodifier/", {
-  sessionid: g_sessionID,
-  appid: "753",
-  assetid: "1309377320",
-  enabled: "1"}).done(function(data) { alert(data.responseText) });
