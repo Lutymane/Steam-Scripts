47ceee53672617453a35e7d13cbeb1b8555f4765
LiteOnE <r89534217610@gmail.com>
2017-09-09 15:15:23 +0300

    Update Extended-Guide-Editing.user.js

--- Extended-Guide-Editing.user.js
+++ Extended-Guide-Editing.user.js
@@ -1,25 +1,39 @@
 // ==UserScript==
 // @name         Extended Guide Editing
-// @version      2.1
-// @description  It allows you to save guide subsections with empty titles and 'unlimited' body text length.
+// @version      2.3
+// @description  Save guide subsections with empty titles and 'unlimited' body text length.
 // @author       Lite_OnE
-// @match        *steamcommunity.com/sharedfiles/editguidesubsection/*
+// @match        *steamcommunity.com/sharedfiles/editguidesubsection*
 // @grant        unsafeWindow
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
 // ==/UserScript==
 
-var location = $(location).attr('href').replace('#', '');
+var location_href = location.href.replace(/#/g, ''),
+    itemid        = location_href.split('?')[1].split('&')[0].split('=')[1],
+    sectionid     = location_href.split("?")[1].split("&")[1].split("=")[1];
 
 $('.btn_green_white_innerfade.btn_small_thin').attr('href', "javascript:ValidateEditExtended();");
-$('#description').attr('maxlength', '1000000000');
+$('#description').removeAttr('maxlength');
 
 unsafeWindow.ValidateEditExtended = function () {
     g_contentChanged = false;
     $.post('//steamcommunity.com/sharedfiles/setguidesubsection', {
-        id : location.split("?")[1].split("&")[0].split("=")[1],
-	sectionid : location.split("?")[1].split("&")[1].split("=")[1],
-	sessionid : g_sessionID,
-	title : title.value,
-	description : description.value});
+        id          : itemid,
+        sectionid   : sectionid,
+        sessionid   : g_sessionID,
+        title       : title.value,
+        description : description.value
+    }).done(function(data){
+        if (data.success == 1)
+        {
+            $('.editGuideSubSectionContentsChangedWarningText').html("Subsection has been saved successfully!");
+        }
+        else
+        {
+            $('.editGuideSubSectionContentsChangedWarningText').html("Unexpected error occured!");
+        }
+    }).fail(function(){
+        $('.editGuideSubSectionContentsChangedWarningText').html("Unexpected error occured!");
+    });
 };
 
