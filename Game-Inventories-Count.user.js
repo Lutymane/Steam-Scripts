924692bf3ae4b5718dccdb32b4bd25af2b0408b6
LiteOnE <r89534217610@gmail.com>
2017-10-02 18:25:56 +0300

    Update Game-Inventories-Count.user.js

--- Game-Inventories-Count.user.js
+++ Game-Inventories-Count.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Game Inventories Count
 // @namespace    #ShortScripts
-// @version      1.2
+// @version      1.3
 // @description  It shows how many game inventories you have
 // @author       Lite_OnE
 // @match        *://steamcommunity.com/*/*/inventory/*
@@ -9,4 +9,4 @@
 // ==/UserScript==
 
 var Count = $J('[id*=inventory_link_]').length;
-$J('.inventory_links').after('<div class="fraud_warning"><div class="fraud_warning_box"><span>Game Inventories Count: ' + Count + '</span></div></div>');
+$J('.games_list_tabs_ctn.responsive_hidden').before('<div class="fraud_warning"><div class="fraud_warning_box"><span>Game Inventories Count: ' + Count + '</span></div></div>');
