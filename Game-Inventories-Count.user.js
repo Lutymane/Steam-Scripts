1f01e0cd3a0a7cb933806c96f809db9d1a273da0
LiteOnE <r89534217610@gmail.com>
2018-02-06 17:56:10 +0300

    Make Broken Inventories Visible

--- Game-Inventories-Count.user.js
+++ Game-Inventories-Count.user.js
@@ -2,12 +2,21 @@
 // @name         Game Inventories Count
 // @icon         https://store.steampowered.com/favicon.ico
 // @namespace    top_xex
-// @version      1.5
+// @version      1.6
 // @description  It shows how many game inventories user has
 // @author       Lite_OnE
 // @match        *://steamcommunity.com/*/*/inventory*
 // @grant        none
 // ==/UserScript==
 
-var Count = $J('[id*=inventory_link_]').length;
-$J('.games_list_tabs_ctn.responsive_hidden').before('<div class="fraud_warning"><div class="fraud_warning_box"><span>Game Inventories Count: ' + Count + '</span></div></div>');
+$J(document).ready(function(){
+    var Inventories = $J('[id*=inventory_link_]');
+    var Count = Inventories.length;
+    $J('.games_list_tabs_ctn.responsive_hidden').before('<div class="fraud_warning"><div class="fraud_warning_box"><span>Game Inventories Count: ' + Count + '</span></div></div>');
+
+    setTimeout(function(){
+        Inventories.each(function(){
+            $J(this).attr('style', 'display:block;');
+        });
+    }, 500);
+});
