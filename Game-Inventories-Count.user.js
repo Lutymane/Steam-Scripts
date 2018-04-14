f41ca940706ee615e367c427c98955eb2ee43409
LiteOnE <r89534217610@gmail.com>
2018-04-15 02:52:58 +0300

    Show total count of associated games on St​eam Inventory Service Group

--- Game-Inventories-Count.user.js
+++ Game-Inventories-Count.user.js
@@ -2,21 +2,33 @@
 // @name         Game Inventories Count
 // @icon         https://store.steampowered.com/favicon.ico
 // @namespace    top_xex
-// @version      1.6
+// @version      1.7
 // @description  It shows how many game inventories user has
 // @author       Lite_OnE
 // @match        *://steamcommunity.com/*/*/inventory*
+// @match        *://steamcommunity.com/groups/InventoryService
 // @grant        none
 // ==/UserScript==
 
-$J(document).ready(function(){
-    var Inventories = $J('[id*=inventory_link_]');
-    var Count = Inventories.length;
-    $J('.games_list_tabs_ctn.responsive_hidden').before('<div class="fraud_warning"><div class="fraud_warning_box"><span>Game Inventories Count: ' + Count + '</span></div></div>');
+var Inventories = null;
+var Count       = 0;
 
-    setTimeout(function(){
+$J(document).ready(function(){
+    if(location.href.includes('InventoryService'))
+    {
+        Count = $J('.group_associated_game_icon').length;
+        $J('.rightbox_content_header').contents()[0].textContent += ' COUNT: ' + Count;
+    }
+    else
+    {
+        Inventories = $J('[id*=inventory_link_]');
+        Count = Inventories.length;
+        $J('.games_list_tabs_ctn.responsive_hidden').before('<div class="fraud_warning"><div class="fraud_warning_box"><span>Game Inventories Count: ' + Count + '</span></div></div>');
+
+        setTimeout(function(){
         Inventories.each(function(){
             $J(this).attr('style', 'display:block;');
         });
-    }, 500);
+        }, 500);
+    }
 });
