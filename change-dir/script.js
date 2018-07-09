e697c34d450386e8c9448234f0c2593c3ff54e5e
LiteOnE <r89534217610@gmail.com>
2018-07-09 23:33:41 +0300

    Update script.js

--- change-dir/script.js
+++ change-dir/script.js
@@ -5,7 +5,7 @@
 // @description  test
 // @author       Lite_OnE
 // @match        https://steamcommunity.com/id/lite_one/
-// @downloadURL  
+// @downloadURL  https://github.com/LiteOnE/Steam-Scripts/blob/test/change-dir/script.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 alert(1);
