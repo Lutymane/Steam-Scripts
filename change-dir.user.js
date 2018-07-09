90aed4ba70b7d25245598810b73a00af47c3a85f
LiteOnE <r89534217610@gmail.com>
2018-07-09 23:31:54 +0300

    Create change-dir.user.js

---
+++ change-dir.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         change-dir-test
+// @namespace    top_xex
+// @version      1.0
+// @description  test
+// @author       Lite_OnE
+// @match        https://steamcommunity.com/id/lite_one/
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+alert(0);
