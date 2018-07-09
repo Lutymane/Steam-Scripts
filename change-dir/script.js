4e34e67f1db084b5734a478266026d6f07b8d215
LiteOnE <r89534217610@gmail.com>
2018-07-09 23:33:31 +0300

    Create script.js

---
+++ change-dir/script.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         change-dir-test
+// @namespace    top_xex
+// @version      1.1
+// @description  test
+// @author       Lite_OnE
+// @match        https://steamcommunity.com/id/lite_one/
+// @downloadURL  
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+alert(1);
