088b310dbe4c3fd5e696f0cd60e3b6eadc3b7e7f
LiteOnE <r89534217610@gmail.com>
2018-07-11 14:43:44 +0300

    Update change-dir.user.js

--- change-dir.user.js
+++ change-dir.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         change-dir-test
 // @namespace    top_xex
-// @version      1.1
+// @version      1.2
 // @description  test
 // @author       Lite_OnE
 // @match        https://steamcommunity.com/id/lite_one/
-// @downloadURL  https://github.com/LiteOnE/Steam-Scripts/blob/test/change-dir/script.js
+// @downloadURL  https://github.com/LiteOnE/Steam-Scripts/raw/test/change-dir/script.user.js
+// @updateURL    https://github.com/LiteOnE/Steam-Scripts/raw/test/change-dir.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 alert(0);
