e0d380dfdd7fd2f410127ccd09be8cf8932cbc7c
LiteOnE <r89534217610@gmail.com>
2018-07-26 16:21:15 +0300

    Create script.meta.js

---
+++ change-dir/script.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         change-dir-test
+// @namespace    top_xex
+// @version      2.1
+// @description  test
+// @author       Lite_OnE
+// @match        https://steamcommunity.com/id/lite_one/
+// @downloadURL  https://github.com/LiteOnE/Steam-Scripts/raw/test/change-dir/script.user.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
