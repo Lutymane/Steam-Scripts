1df528072609be2d04278e8118844c3480904014
LiteOnE <r89534217610@gmail.com>
2018-07-26 16:23:06 +0300

    Update script.meta.js

--- change-dir/script.meta.js
+++ change-dir/script.meta.js
@@ -1,11 +1,4 @@
 // ==UserScript==
 // @name         change-dir-test
-// @namespace    top_xex
 // @version      2.1
-// @description  test
-// @author       Lite_OnE
-// @match        https://steamcommunity.com/id/lite_one/
-// @updateURL    https://github.com/LiteOnE/Steam-Scripts/raw/test/change-dir/script.meta.js
-// @downloadURL  https://github.com/LiteOnE/Steam-Scripts/raw/test/change-dir/script.user.js
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
